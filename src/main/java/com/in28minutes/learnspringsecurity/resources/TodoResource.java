afd0ac9573c893883f9b0754939e75101663f7cc
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2023-04-01 20:23:00 +0700

    Step 08 - Exploring Spring Security - Cross Site Request Forgery - CSRF

---
+++ src/main/java/com/in28minutes/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,34 @@
+package com.in28minutes.learnspringsecurity.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+    private final List<Todo> TODOS_LIST = List.of(new Todo("in28minutes", "Learn AWS"), new Todo("in28minutes", "Get AWS Certified"));
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos() {
+        return TODOS_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
+        return TODOS_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo) {
+//        TODOS_LIST.add(todo);
+        logger.info("Create {} for {}", todo, username);
+    }
+
+}
+
+record Todo(String username, String description) {
+
+}
