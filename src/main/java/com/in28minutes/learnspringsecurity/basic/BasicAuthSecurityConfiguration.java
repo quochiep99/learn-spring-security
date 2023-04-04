55efed25ab73f11912d1607a53dd65da16706950
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2023-04-04 23:55:25 +0700

    Step 13 - Explore Spring Security - Storing User Credentials using JDBC

--- src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,13 +2,19 @@ package com.in28minutes.learnspringsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -19,11 +25,12 @@ public class BasicAuthSecurityConfiguration {
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic();
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    UserDetailsManager userDetailsManager(DataSource dataSource) {
         UserDetails user = User.builder()
                 .username("user")
                 .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
@@ -34,6 +41,19 @@ public class BasicAuthSecurityConfiguration {
                 .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
                 .roles("USER", "ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
+
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+
 }
